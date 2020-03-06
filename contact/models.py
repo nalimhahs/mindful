08ad80bd6c342715eee1eb3ba3566f7bf3108ab9
anit <anitpaulroslin@gmail.com>
2020-03-06 11:01:18 +0530

    contacts page completed

--- contact/models.py
+++ contact/models.py
@@ -4,6 +4,8 @@ class Contact(models.Model):
     name=models.CharField(max_length=30)
     address=models.TextField(max_length=60)
     phoneno=models.IntegerField()
+    image = models.ImageField()
+
 
     def __str__(self):
         return self.name
--- contact/views.py
+++ contact/views.py
@@ -3,4 +3,4 @@ from .models import Contact
 
 def contacts(request):
     contacts = Contact.objects
-    return render(request, 'contact/contacts.html', {'contacts': contacts})
+    return render(request, 'contacts/contacts.html', {'contacts': contacts})
