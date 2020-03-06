cfb27896a655d11f935373ffd0d2e0c8ef4d11b5
Reenu Maria Jerry <54211230+reenu153@users.noreply.github.com>
2020-03-06 09:57:54 +0400

    weighted average of analysed data

--- socialanalysis/views.py
+++ socialanalysis/views.py
@@ -13,7 +13,26 @@ def analyseSocialsView(request):
             username = form.cleaned_data['username']
             tweets = get_all_tweets(username)
             # print(tweets)
-            analysis = datreeINPUT(tweets[0][2])
+            positive=0
+            negative=0
+            neutral=0
+            point=20
+            for i in range(0,20): 
+                analysis = datreeINPUT(tweets[i][2])
+                if analysis == "positive":
+                    positive+=point
+                elif analysis == "neutral":
+                    neutral+=point
+                else: 
+                    negative+=point
+                point-=1
+            if positive>negative and positive>neutral:
+                analysis="positive"
+            elif negative>positive and negative>neutral:
+                analysis="negative"
+            else:
+                analysis="neutral"
+                
     else:
         form = UsernameForm()
 
