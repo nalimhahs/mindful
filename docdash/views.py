d620748f60c00e767ce50ac41a6301bd89f654a8
Milan Shah <milanshah1@gmail.com>
2020-03-06 11:24:11 +0530

    doc dash and patient data updtae

--- docdash/views.py
+++ docdash/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from users.decorators import is_doctor
 from forum.models import Post, Thread
-
+from livechat.models import ChatRoom, Chat
+from datetime import datetime, timedelta
 
 @login_required
 @is_doctor
@@ -10,6 +11,16 @@ def doctorMainDashboard(request):
 
     active_threads = Thread.objects.filter(
         post__in=Post.objects.filter(user=request.user)).order_by('-created')
+    patient_count = ChatRoom.objects.filter(doctor=request.user)
+    # availability_status_of _doctor
+    recent_chats = ChatRoom.objects.filter(doctor=request.user)
+    # patiient_sentiment
+    # new_stuff_to_publish
+    # approval_stuff_for_chats
+    patient_interaction = []
+    for i in range(7):
+        patient_interaction.append(Chat.objects.filter(room__doctor=request.user, created=datetime.now()-timedelta(days=i)))
+    # doc_rating
 
     return render(request, 'docdash/home.html', {'active_threads': active_threads})
 
